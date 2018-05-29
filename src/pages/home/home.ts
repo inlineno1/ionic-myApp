81c6b0dc477497d09cfd57cb9f5ebbc7dfb69277
James <inlineno1@gmail.com>
2018-05-29 21:54:52 +0900

    2days

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,61 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+/**
+ * Generated class for the HomePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+// @IonicPage({name : 'homePage'})
+@IonicPage()
 @Component({
+
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  disabledSwitch:boolean = false;
+  userName:any;
+
+  items = [
+    {"id":1,"name":"첫번쩨 아이템"},
+    {"id":2,"name":"두번쩨 아이템"},
+    {"id":3,"name":"세번쩨 아이템"},
+    {"id":4,"name":"네번쩨 아이템"}
+  ];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HomePage');
+  }
+
+  ionViewDidLeave() {
+    console.log('ionViewDidLeave HomePage');
+  }
+
+  itemClicked(item) {
+    alert(item.name);
+
+    // let toast = this.toastCtrl.create({
+    //   message: item.name,
+    //   duration: 3000,
+    //   position: 'bottom'
+    // });
+    //
+    // toast.present();
 
   }
 
+  buttonClicked(event) {
+    // alert(event.target.textContent + " " + event.currentTarget.value);
+    // bind 페이지 호출
+    this.navCtrl.push('BindPage', {value:event.currentTarget.value});
+
+    console.log(event.clientX + " " + event.clientY);
+  }
 }
